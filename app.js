96831332255d9a21337581f9cb913885d2b2062c
guptamanas149@gmail.com <guptamanas149@gmail.com>
2022-10-11 23:53:27 +0530

    made ejs files

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-const {Tag} = require("./model");
+const path = require("path");
+const mongoose = require("mongoose");
+const { Tag } = require("./model");
 
-const app=express();
+const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}));
-mongoose.connect("mongodb+srv://admin:pVzqSUxXFKLATeVw@cluster0.lelomd6.mongodb.net/?retryWrites=true&w=majority",
-    { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
+// app.set("public_html", path.resolve(__dirname, "public"));
+app.use("/", express.static(__dirname + "/public"));
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:pVzqSUxXFKLATeVw@cluster0.lelomd6.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }
-).then(()=>{
+  )
+  .then(() => {
     console.log("connected to db");
-});
+  });
 
-app.use(async (req, res, next)=>{
-    req.tags = await Tag.find({}, {name: 1, _id:0});
-    return next();
-})
+app.use(async (req, res, next) => {
+  req.tags = {};
+  tags = await Tag.find({}, { name: 1, _id: 0 });
+  req.tags.name = [];
+  req.tags.links = [];
+  req.tags.length = tags.length;
+  for (let i = 0; i < tags.length; i++) {
+    req.tags.name.push(tags[i].name);
+    req.tags.links.push(tags[i].name.replace(/\s/g, "+"));
+  }
+  return next();
+});
 let adminRoutes = require("./adminRoutes");
 app.use("/admin", adminRoutes);
 
 let userroutes = require("./userRoutes");
 app.use("/", userroutes);
 
-app.listen(process.env.PORT||8080);
+app.listen(process.env.PORT || 8080);
--- userRoutes.js
+++ userRoutes.js
@@ -1,7 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
-const {Post, Tag} = require("./model");
+const { Post, Tag } = require("./model");
 
 const DEFAULT_QUERY_PAGE = 1;
 const DEFAULT_QUERY_PERPAGE = 10;
@@ -11,33 +10,45 @@ const DEFAULT_QUERY_PERPAGE = 10;
 //     return res.status(200).json(tags);
 // }
 
-async function getPaginatedPosts(req, res){
-    let data ={};
-    data.tags = req.tags;
-    const query = {
-        // "tag.name" : req.params.tag,
-    };
-    req.params.tag = req.params.tag.replace(/\+/g, " ")
-    let options = {
-        populate: {
-            path: "tag",
-            select : "name -_id",
-            match: {
-                "name": req.params.tag
-            }
-        },
-        select:"title desc links people tag updatedAt",
-        order:{
-            "updatedAt": -1            
-        }
-    }
-    data.posts = await Post.find(query).select(options.select).populate(options.populate).sort(options.order);
-    data.posts = data.posts.filter((post)=>{return post.tag!==null;})
-    return res.status(200).json(data);
+async function getPaginatedPosts(req, res) {
+  let data = {};
+  data.tags = req.tags;
+  const query = {
+    // "tag.name" : req.params.tag,
+  };
+  req.params.tag = req.params.tag.replace(/\+/g, " ");
+  let options = {
+    populate: {
+      path: "tag",
+      select: "name -_id",
+      match: {
+        name: req.params.tag,
+      },
+    },
+    select: "title desc links people tag updatedAt",
+    order: {
+      updatedAt: -1,
+    },
+  };
+  data.posts = await Post.find(query)
+    .select(options.select)
+    .populate(options.populate)
+    .sort(options.order);
+  data.posts = data.posts.filter((post) => {
+    return post.tag !== null;
+  });
+  data.postlength = data.posts.length;
+  return res.render("topic", data);
 }
 
+function home(req, res) {
+  let data = {};
+  data.tags = req.tags;
+  return res.render("index", data);
+}
 
 // router.get("/tags", getAllTags);
 router.get("/posts/:tag", getPaginatedPosts);
+router.get("/", home);
 
 module.exports = router;
